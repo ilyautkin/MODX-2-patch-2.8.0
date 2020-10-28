3685240cad4a72ccf77283f94b53ffa7ef06daf9
Ilya Utkin <ilyautkin@mail.ru>
2020-10-28 12:04:33 +0300

    Change patch name

--- _build/resolvers/remove_patch.php
+++ _build/resolvers/remove_patch.php
@@ -1,5 +1,5 @@
 <?php
-unlink(MODX_CORE_PATH . 'packages/modx-2.8.0-patch-1.0.1-pl.transport.zip');
+unlink(MODX_CORE_PATH . 'packages/modx2-2.8.0-patch-1.0.1-pl.transport.zip');
 if ($cache = $transport->xpdo->getCacheManager()) {
-    $cache->deleteTree(MODX_CORE_PATH . 'packages/modx-2.8.0-patch-1.0.1-pl/', ['deleteTop' => true, 'skipDirs' => false, 'extensions' => []]);
+    $cache->deleteTree(MODX_CORE_PATH . 'packages/modx2-2.8.0-patch-1.0.1-pl/', ['deleteTop' => true, 'skipDirs' => false, 'extensions' => []]);
 }
